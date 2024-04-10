9b55fdc091a718777e86e2362b09883431fa6049
Caio <caiogb1998@gmail.com>
2024-04-09 23:49:55 -0300

    insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -36,12 +36,13 @@ public class SellerDaoJDBC implements SellerDao {
 
             int rowsAffected = st.executeUpdate();
 
-            if (rowsAffected <0){
+            if (rowsAffected > 0){
                 ResultSet rs = st.getGeneratedKeys();
                 if (rs.next()){
                     int id = rs.getInt(1);
                     obj.setId(id);
                 }
+                DB.closeResultSet(rs);
             }else {
                 throw new DbException("Unexpected error! No rows affected!");
             }
